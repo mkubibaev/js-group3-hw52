3dde618ff57ed02a406d69d26105063081ab1749
mkuibaev <mkuibaev@gmail.com>
2018-12-15 21:14:41 +0600

    added getNewNumbers function

--- src/App.js
+++ src/App.js
@@ -3,10 +3,33 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+	state = {
+		numbers: []
+	};
+
+	getNewNumbers = () => {
+		const numbers = [];
+
+		while(true) {
+			const randomNum = Math.floor(Math.random() * 32 + 5);
+			if (!numbers.includes(randomNum)) {
+				numbers.push(randomNum);
+			}
+			if (numbers.length === 5) {
+				break;
+			}
+		}
+
+		numbers.sort((a, b) => a - b);
+		this.setState({numbers});
+
+		console.log(this.state.numbers);
+	};
+
 	render() {
 		return (
 			<div className="App">
-
+				<button onClick={this.getNewNumbers}>New numbers</button>
 			</div>
 		);
 	}
